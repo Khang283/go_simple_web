0020119e70d812e382d2df79e0daea1d7a2ddf71
Khang283 <phanhoangbaokhang2803@gmail.com>
2024-11-17 14:32:59 +0700

    updated github action

--- daos/class.go
+++ daos/class.go
@@ -1,4 +1,34 @@
 package daos
 
-func GetClassList() {
+import (
+	"context"
+	"crud/types"
+	"log"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func GetClassList() []types.Class {
+	dbpool, err := pgxpool.New(context.Background(), *DATABASE_URL)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer dbpool.Close()
+	classses := make([]types.Class, 0)
+	query := "select id, name, description, type, code from class"
+	rows, err := dbpool.Query(context.Background(), query)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	for rows.Next() {
+		var class types.Class
+		if err = rows.Scan(&class.Id, &class.Name, &class.Description, &class.Type, &class.Code); err != nil {
+			log.Fatal(err.Error())
+		}
+		classses = append(classses, class)
+	}
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	return classses
 }
